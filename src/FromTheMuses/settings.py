8cf15fd7c62644e34a176d1dc000fd4cf11ea5fe
Brian <zhub0531@hotmail.com>
2020-06-25 16:04:28 -0400

    azure setup

--- src/FromTheMuses/settings.py
+++ src/FromTheMuses/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&&jdho^miacl34rnz&=!_ow^^w24z2l9l4zr^3bj&70iwc53uj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
