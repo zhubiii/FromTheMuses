89c3c864cd26abb4180fbb8493db2997aec20ef7
Brian <zhub0531@hotmail.com>
2020-06-25 11:58:01 -0400

    about and contact pages

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import (
+    HomePageView,
+    AboutPageView,
+    ContactPageView
+)
 
 app_name = 'pages'
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('about',AboutPageView.as_view(),name='about'),
+    path('contact',ContactPageView.as_view(),name='contact')
 ]
--- src/pages/views.py
+++ src/pages/views.py
@@ -4,3 +4,9 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
