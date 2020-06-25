bf486647a4739b0850c152a5bdb9e3b9a15d1a71
Brian <zhub0531@hotmail.com>
2020-06-25 11:12:51 -0400

    nicer dir organization, class based views

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 # Create your views here.
-def home_view(request,*args,**kwargs):
-    #print(request.user)
-    return render(request,"home.html",{})
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
