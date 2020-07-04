d030190d696a0bd92eece3fdc687fab9e8d843a5
Brian <zhub0531@hotmail.com>
2020-07-04 15:41:45 -0400

    navbar changes based on registerd user or not

--- register/urls.py
+++ register/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import register_form_view
+from .views import register_form_view,RegisterSuccessPageView
 
 app_name = 'register'
 urlpatterns = [
-    path('', register_form_view, name='register-form')
+    path('', register_form_view, name='register-form'),
+    path('registration-successful', RegisterSuccessPageView.as_view(), name='registration-success')
+
 ]
--- register/views.py
+++ register/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 def register_form_view (response):
@@ -7,7 +9,10 @@ def register_form_view (response):
         form = RegisterForm(response.POST)
         if form.is_valid():
             form.save()
-            return redirect("/home")
+            return redirect("/register/registration-successful")
     else:
         form = RegisterForm()
     return render(response,"register/register_form.html",{"form":form})
+
+class RegisterSuccessPageView(TemplateView):
+    template_name = 'register/register_success.html'
