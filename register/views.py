3123908a85b5754d4073a0134a7ce1ca5148e546
Brian <zhub0531@hotmail.com>
2020-07-05 11:44:42 -0400

    user logged in automatically upon registration

--- register/views.py
+++ register/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def register_form_view (response):
@@ -9,6 +10,8 @@ def register_form_view (response):
         form = RegisterForm(response.POST)
         if form.is_valid():
             form.save()
+            new_user = authenticate(username=form.cleaned_data['username'],password=form.cleaned_data['password1'],)
+            login(response,new_user)
             return redirect("/register/registration-successful")
     else:
         form = RegisterForm()
